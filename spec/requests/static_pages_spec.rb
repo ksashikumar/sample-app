613054b4aae2c7f227d279bf1a3b50dce5850bbb
ksashikumar <ksashikumark93@gmail.com>
2016-02-05 14:47:23 +0530

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,14 +6,39 @@ describe "Static pages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => "Sample App")
+    end
+
+    it "should have the title 'Home'" do 
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Sample App | Home")
     end
   end
 
   describe "Help page" do 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => "Help")
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help' 
+      page.should have_selector('title', :text => "Ruby on Rails Sample App | Help")
+    end
+
   end
+
+  describe "About page" do
+    it "should have content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => "About Us")
+    end
+
+    it "should have title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Ruby on Rails Sample App | About Us")
+    end
+
+  end
+
 end
