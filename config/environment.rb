327e24e738379bcb49f9fc5cc315865303a0fbe6
ksashikumar <ksashikumark93@gmail.com>
2016-02-12 13:08:44 +0530

    add rack cache

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+config.middleware.use Rack::Cache,
+  verbose:     true,
+  metastore:   'file:/var/cache/rack/meta',
+  entitystore: 'file:/var/cache/rack/body'
